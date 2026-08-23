5a2f313ce3e8d4303162d965a5d67a6c43b89e23
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-08-23 12:50:35 -0300

    comented ReactQueryDevtools

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import './styles/global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+{/*import { ReactQueryDevtools } from '@tanstack/react-query-devtools'*/}
 import App from './App'
 
 const queryClient = new QueryClient({
@@ -18,7 +18,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </StrictMode>,
 )
